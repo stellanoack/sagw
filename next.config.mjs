236f9bb4fc171f75fdfefa115f8b994a49c87d35
Yves Tscherry <info@feerglas.com>
2025-03-26 18:33:56 +0100

    chore: fix absolute imports for scss

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
+import * as path from 'path';
+import { fileURLToPath } from 'url';
 import { withPayload } from '@payloadcms/next/withPayload';
 
+const rootFileName = fileURLToPath(import.meta.url);
+const rootDirName = path.dirname(rootFileName);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.resolve(rootDirName, 'src/styles')],
+  },
+};
 
 export default withPayload(nextConfig, {
   devBundleServerPackages: false,
