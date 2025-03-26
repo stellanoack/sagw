5f1ea7757babd9e3fa3a0caf715948154bbcc1fb
Yves Tscherry <info@feerglas.com>
2025-03-26 11:29:29 +0100

    chore: remove iframe from base url

--- src/components/config.ts
+++ src/components/config.ts
@@ -1,6 +1,6 @@
 export const componentsConfig = {
   storybookRootDivId: 'storybook-root',
   testid: 'component',
-  vrtBaseUrl: 'http://127.0.0.1:6006/iframe.html',
+  vrtBaseUrl: 'http://127.0.0.1:6006',
   vrtSnapshotFolder: 'src/components/snapshots',
 };
