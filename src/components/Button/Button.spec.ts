f2b07b1584c1fcb5c9b72506846d93f2d0928ba6
Yves Tscherry <info@feerglas.com>
2025-03-28 12:18:45 +0100

    chore: write button wit capital start letter

---
+++ src/components/Button/Button.spec.ts
@@ -0,0 +1,16 @@
+import {
+  expect,
+  test,
+} from '@playwright/test';
+import { navigate } from '@/testing-helpers';
+
+test('button should be visible', async ({
+  page,
+}) => {
+  await navigate(page, 'components-button--small');
+
+  const elem = await page.getByRole('button');
+
+  await expect(elem)
+    .toBeVisible();
+});
