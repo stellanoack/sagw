6af70e7b609a92650854fd8de8fdabe202f2b24f
Yves Tscherry <info@feerglas.com>
2025-03-28 08:58:24 +0100

    chore: use global ignores, use meta dirname

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,18 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 import tsRules from './lint/ts-rules.mjs';
 import esRules from './lint/es-rules.mjs';
-
-const filename = fileURLToPath(import.meta.url);
-const rootDirname = dirname(filename);
+import { globalIgnores } from 'eslint/config';
 
 const compat = new FlatCompat({
-  baseDirectory: rootDirname,
+  baseDirectory: import.meta.dirname,
 });
 
 const eslintConfig = [
-  {
-    ignores: [
-      '**/app/(payload)/**',
-      '**/convenience/**',
-    ],
-  },
+  globalIgnores([
+    '**/app/(payload)/*',
+    '**/convenience/*',
+    '**/node_modules/*',
+  ]),
   ...compat.extends('next/core-web-vitals', 'next/typescript', 'plugin:storybook/recommended'),
   {
     files: [
